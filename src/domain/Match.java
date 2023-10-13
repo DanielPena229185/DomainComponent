ebe5ebff7595173a08dd20adf8edea99879eb53f
DanielPenaGarcia <dapgpena@gmail.com>
2023-10-12 20:45:50 -0700

    Entitad ficha finalizada

--- src/domain/Match.java
+++ src/domain/Match.java
@@ -1,22 +1,25 @@
 /**
-* Tile.java
-* Oct 12, 2023 6:00:01 PM
-*/ 
-
+ * Match.java
+ * Oct 12, 2023 5:59:39 PM
+ */
+package domain;
 
 /**
- *
  * @author Daniel Armando Peña Garcia ID:229185
  * @author Santiago Bojórquez Leyva ID:228475
  * @author Paul Alejandro Vazquez Cervantes ID:241400
  * @author Jose Eduardo Hinojosa Romero ID: 2356666
  */
-public class Tile {
+public class Match {
+
+    private Player[] players;
+    private Board board;
+    private Pool pool;
 
     /**
-     *
+     * Default constructor
      */
-    public Tile(){
-
+    public Match() {
     }
+
 }
