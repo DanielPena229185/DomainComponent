8b6d8d19c512e8ef537121d8df925f28a2ed5ad6
DanielPenaGarcia <dapgpena@gmail.com>
2023-10-12 21:38:10 -0700

    Se realizó la definición de la clase GameElement

--- src/domain/Match.java
+++ src/domain/Match.java
@@ -4,22 +4,43 @@
  */
 package domain;
 
+import interfaces.Game;
+
 /**
  * @author Daniel Armando Peña Garcia ID:229185
  * @author Santiago Bojórquez Leyva ID:228475
  * @author Paul Alejandro Vazquez Cervantes ID:241400
  * @author Jose Eduardo Hinojosa Romero ID: 2356666
  */
-public class Match {
+public class Match implements Game{
 
+    /**
+     * An array of Player objects representing the participants in the match.
+     */
     private Player[] players;
+
+    /**
+     * The game board where the match takes place.
+     */
     private Board board;
+
+    /**
+     * The pool of game pieces or tiles used in the match.
+     */
     private Pool pool;
 
     /**
-     * Default constructor
+     * Initializes a new Match with the specified players, board, and pool.
+     *
+     * @param players An array of Player objects representing the participants
+     * in the match.
+     * @param board The game board where the match takes place.
+     * @param pool The pool of game pieces or tiles used in the match.
      */
-    public Match() {
+    public Match(Player[] players, Board board, Pool pool) {
+        this.players = players;
+        this.board = board;
+        this.pool = pool;
     }
 
 }
--- src/domain/Player.java
+++ src/domain/Player.java
@@ -4,18 +4,31 @@
  */
 package domain;
 
+import interfaces.Game;
+
 /**
  * @author Daniel Armando Peña Garcia ID:229185
  * @author Santiago Bojórquez Leyva ID:228475
  * @author Paul Alejandro Vazquez Cervantes ID:241400
  * @author Jose Eduardo Hinojosa Romero ID: 2356666
  */
-public class Player {
+public class Player extends GameElement {
 
     /**
-     *
+     * Name of the player
      */
-    public Player() {
+    private String name;
 
+    /**
+     * Constructs a Player with the specified name and associates it with the
+     * given Game.
+     *
+     * @param name The name of the player.
+     * @param game The game to which the player belongs.
+     */
+    public Player(String name, Game game) {
+        super(game);
+        this.name = name;
     }
+
 }
